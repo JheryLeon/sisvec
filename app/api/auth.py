d4e72b667c4b59912b26bb85418cbe13325476ec
Jhery <leoncarballo354@gmail.com>
2026-06-02 20:26:52 -0400

    chore: remover import session no usado

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,6 +1,6 @@
 import traceback, os, time, secrets
 from datetime import datetime, timezone
-from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify, current_app, make_response, session
+from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify, current_app, make_response
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.utils import secure_filename
 
